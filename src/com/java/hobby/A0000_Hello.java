d212d8087f365654f8a7ea56280af442aa367dde
Ben De Leon <bendeleonjr@hotmail.com>
2021-12-24 16:45:28 +0800

    basic print

--- src/com/java/hobby/A0000_Hello.java
+++ src/com/java/hobby/A0000_Hello.java
@@ -3,5 +3,6 @@ package com.java.hobby;
 public class A0000_Hello {
     public static void main(String[] args){
         System.out.println("Hello, people!");
+        System.out.println("Hello, world!");
     }
 }
